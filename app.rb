d18dc6c95939ebf3d18e955e3965ad3afa00ae20
ClemenY <yangyang.kadk@gmail.com>
2017-10-19 20:46:07 -0400

    fix

--- app.rb
+++ app.rb
@@ -103,7 +103,7 @@ config.access_token_secret = "LPSgx0m7pOkBwGOe54IJqBiiYBZ7UTxRm0ZTs5NOuJkAU"
 end
 
 
-search tweets around cmu campus mention German related topics
+#search tweets around cmu campus mention German related topics
 search_options = {result_type: "recent",geocode: "40.4434119,-79.9429875,10mi"}
 client.search("#Berlin", search_options).take(1).each do |tweet|
  puts "#{tweet.user.screen_name}: #{tweet.text}"
